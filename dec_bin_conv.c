1393278fc0bfce1cf777f7047cc63dd3b1fad99f
Nidhi-KM40BESD18 <nc7698269@gmail.com>
2021-08-31 20:21:36 +0530

    Modified dec_bin_conv.c

--- dec_bin_conv.c
+++ dec_bin_conv.c
@@ -1,22 +1,39 @@
 
-
 #include<stdio.h>
+void dec_bin(int *x)
+{
+	int i;
+	for(i=0; i<(sizeof(*x))*8; i++)
+        {
+                if(*x & 0x80000000)
+                        printf("1");
+                else
+                        printf("0");
+                *x=*x << 1;
+        }
+
+}
 int main()
 {
         int i,temp;
         int x;
         temp= sizeof(x);
         printf("Enter a number\n");
-        scanf("%d",&x);
-        for(i=0; i<temp*8; i++)
-        {
-                if(x & 0x80000000)
-                        printf("1");
-                else
-                        printf("0");
-                x=x << 1;
-        }
+        scanf("%u",&x);
+	if(x >= 0)
+	{
+	dec_bin(&x);
         printf("\n");
+	}
+	else
+	{
+		int z;
+		x=(-x);
+		z= ~x;
+		z += 1;
+		dec_bin(&z);
+		printf("\n");
+	}
         return 0;
 }
 
